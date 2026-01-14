74b0c831e537a9728ca9080d85854957eb0b31ba
TaeYoung27 <qkdtom@naver.com>
2026-01-14 16:54:53 +0900

    라우트 파일 수정 다시

--- Ai/app/api/v1/routes/router.py
+++ Ai/app/api/v1/routes/router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from Ai.app.api_v1.routes.health import router as health_router
-from Ai.app.api_v1.routes.anomaly import router as anomaly_router
-from Ai.app.api_v1.routes.wear_factor import router as wear_factor_router
-from Ai.app.api_v1.routes.visual_router import router as visual_router  # 네 프로젝트에 이미 있으면
+from ai.app.api.v1.routes.health import router as health_router
+from ai.app.api.v1.routes.anomaly import router as anomaly_router
+from ai.app.api.v1.routes.wear_factor import router as wear_factor_router
+from ai.app.api.v1.routes.visual_router import router as visual_router  # 네 프로젝트에 이미 있으면
 
 router = APIRouter()
 
