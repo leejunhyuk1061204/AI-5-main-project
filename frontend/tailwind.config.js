4d28f15ce25bd2dea8ace4896d4947396758ded5
최최성빈 <csb3686@gmail.com>
2026-01-16 15:31:49 +0900

    풀 받기 위한 푸쉬

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require("nativewind/preset")],
+  presets: [require("nativewind/dist/tailwind")],
   content: ["./**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
